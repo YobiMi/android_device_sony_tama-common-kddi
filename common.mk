7f32f929c622bc72690c5b40ebb8b04f58c948cb
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    tama-common-kddi: Pick up USB configurations from QCOM
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -426,6 +426,8 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    vendor/qcom/opensource/wfd-commonsys \
+    vendor/qcom/opensource/usb/etc \
     hardware/google/pixel
 
 # Telephony
