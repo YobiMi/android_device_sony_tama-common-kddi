735d8257c3c20de6f9a5b73d167001fb00e78e1d
ekkusa <dayesofficial@gmail.com>
2022-09-05 13:08:45 +0000

    tama-common-kddi: uprev audiohal from 7.0 to 7.1

--- common.mk
+++ common.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     audio.r_submix.default \
     audio.primary.default \
