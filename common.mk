953005cfe0594bc29becb500c4ed524ff17ba0ad
ekkusa <dayesofficial@gmail.com>
2022-09-10 14:21:40 +0000

    tama-common-kddi: Build missing Soundtrigger

--- common.mk
+++ common.mk
@@ -103,6 +103,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.r_submix.default \
