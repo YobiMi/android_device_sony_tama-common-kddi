7622159b41cbb365fed615cce10810594613ea38
ekkusa <dayesofficial@gmail.com>
2022-09-10 15:59:51 +0000

    tama-common-kddi: changes

--- common.mk
+++ common.mk
@@ -100,8 +100,6 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
