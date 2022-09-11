923fad4ef8f50c4c4866a20fce51c1019f3136ce
ekkusa <dayesofficial@gmail.com>
2022-09-11 07:28:58 +0000

    tama-common-kddi: drop a2dp
    deprecated in a13

--- common.mk
+++ common.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.primary.default \
     audio.usb.default \
