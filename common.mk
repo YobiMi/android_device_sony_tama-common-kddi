06e5f0337f59a24cfefd3cf9c6b1fb23abbde1bb
ekkusa <dayesofficial@gmail.com>
2022-09-12 21:51:19 +0000

    tama-common-kddi: fixes

--- common.mk
+++ common.mk
@@ -390,7 +390,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.sony_sdm845-libperfmgr \
-    android.hardware.power@1.3.vendor
+    android.hardware.power@1.3.vendor \
+    android.hardware.power@1.0
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
